7426cc565f3e4d519cb763e257e22b307032a2d7
anzr299 <aamir.nazir@intel.com>
2026-04-07 15:48:51 +0400

    add from_builder.. function in init

--- backends/openvino/quantizer/__init__.py
+++ backends/openvino/quantizer/__init__.py
@@ -1,4 +1,4 @@
-from .llm_compression import apply_nncf_data_aware_compression
+from .llm_compression import apply_nncf_data_aware_compression, apply_nncf_data_aware_compression_from_builder
 from .quantizer import OpenVINOQuantizer, QuantizationMode, quantize_model
 
 __all__ = [
@@ -6,4 +6,5 @@ __all__ = [
     "quantize_model",
     "QuantizationMode",
     "apply_nncf_data_aware_compression",
+    "apply_nncf_data_aware_compression_from_builder",
 ]
